bb43f8c0d5a1080cde5f140fd4353cfce61a868b
Austin Akers <austin.akers5@gmail.com>
2016-01-25 22:42:18 -0600

    added JS file,JSON files,altered .html files
    made heavy modifications to the html pages and added the JavaScript
    functionality.

---
+++ Project1/script.js
@@ -0,0 +1,39 @@
+var app = angular.module("computer",['ngRoute'])
+
+.config(['$routeProvider', function($routeProvider){
+  $routeProvider.
+    when('/main',{
+      templateUrl: 'main.html',
+      controller: 'MainCtrl'
+    }).
+    when('/about',{
+      templateUrl: 'about.html',
+      controller: 'MainCtrl'
+    }).
+    when('/services',{
+      templateUrl: 'services.html',
+      controller: 'ServicesCtrl'
+    }).
+    when('/contact',{
+      templateUrl: 'contact.html',
+      controller: 'ContactCtrl'
+    }).
+    otherwise({redirectTo:'/main'})
+}])
+
+.controller('MainCtrl',['$scope', '$http', function($scope, $http){
+  $http.get('services.json').then(function(response){
+    $scope.services = response.data;
+  });
+}])
+
+.controller('ServicesCtrl',['$scope','$http', function($scope, $http){
+  $http.get('services.json').then(function(response){
+    $scope.services = response.data;
+  });
+}])
+.controller('ContactCtrl',['$scope','$http' function($scope, $http){
+  $http.get('locations.json').then(function(response){
+    $scope.locations = response.data;
+  });
+}]);
