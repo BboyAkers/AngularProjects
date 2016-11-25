a35bd125ce8ac83c98dab70abdb779c20a707a60
Austin Akers <austin.akers5@gmail.com>
2016-11-25 00:41:50 -0600

    Added profile component and modified app files

--- GithubSearch/app/app.module.ts
+++ GithubSearch/app/app.module.ts
@@ -3,9 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent }  from './app.component';
 
+import { ProfileComponent }  from './components/profile.component';
+
 @NgModule({
   imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
+  declarations: [ AppComponent, ProfileComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ GithubSearch/app/components/profile.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'profile',
+  templateUrl: `profile.component.html`
+})
+export class ProfileComponent  {  }
