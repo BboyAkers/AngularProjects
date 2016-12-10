316dd56a0e68c69d5d245887a8c11a9cc694e841
Austin Akers <austin.akers5@gmail.com>
2016-12-10 01:37:29 -0600

    Added  Search functionality

--- ng2-Spotify/app/app.module.ts
+++ ng2-Spotify/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule }   from '@angular/forms';
+import {HttpModule, Http}    from '@angular/http';
 import { AppComponent }  from './app.component';
 
 import {SearchComponent} from './components/search/search.component';
 import {NavbarComponent} from './components/navbar/navbar.component';
-import {AboutComponent} from './components/about/about.component';
+import {AboutComponent}  from './components/about/about.component';
 
 import {routing} from './app.routing';
 
 @NgModule({
-  imports:      [ BrowserModule, routing ],
+  imports:      [
+    BrowserModule,
+    routing,
+    FormsModule,
+    HttpModule
+  ],
   declarations: [
     AppComponent,
     SearchComponent,
