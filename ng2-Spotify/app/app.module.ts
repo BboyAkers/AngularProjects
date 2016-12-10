881eff308846b83f82a70a9a0fae6774b71e6b94
Austin Akers <austin.akers5@gmail.com>
2016-12-10 12:50:04 -0600

    Added Artist Component and Route

--- ng2-Spotify/app/app.module.ts
+++ ng2-Spotify/app/app.module.ts
@@ -1,28 +1,32 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
-import {HttpModule, Http}    from '@angular/http';
+import { HttpModule }    from '@angular/http';
 import { AppComponent }  from './app.component';
 
-import {SearchComponent} from './components/search/search.component';
-import {NavbarComponent} from './components/navbar/navbar.component';
-import {AboutComponent}  from './components/about/about.component';
+import { SearchComponent } from './components/search/search.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { AboutComponent }  from './components/about/about.component';
+import { ArtistComponent } from './components/artist/artist.component';
 
 import {routing} from './app.routing';
 
 @NgModule({
-  imports:      [
-    BrowserModule,
-    routing,
-    FormsModule,
-    HttpModule
-  ],
-  declarations: [
-    AppComponent,
-    SearchComponent,
-    NavbarComponent,
-    AboutComponent
-  ],
+  imports:
+    [
+      BrowserModule,
+      routing,
+      FormsModule,
+      HttpModule
+    ],
+  declarations:
+    [
+      AppComponent,
+      SearchComponent,
+      NavbarComponent,
+      AboutComponent,
+      ArtistComponent
+    ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
--- ng2-Spotify/app/services/spotify.service.ts
+++ ng2-Spotify/app/services/spotify.service.ts
@@ -6,6 +6,7 @@ import 'rxjs/add/operator/map';
 @Injectable()
 export class SpotifyService {
   private searchUrl: string;
+  private artistUrl: string;
 
   constructor(private _http:Http){
 
@@ -14,6 +15,12 @@ export class SpotifyService {
   searchMusic(str:string, type='artist'){
     this.searchUrl = 'https://api.spotify.com/v1/search?query=' + str + '&offset=0&limit=20&type=' + type + '&market=US';
     return this._http.get(this.searchUrl)
+      .map(res => res.json());
+  }
+
+  getArtist(id:string){
+    this.artistUrl = 'https://api.spotify.com/v1/artists/' + id;
+    return this._http.get(this.artistUrl)
       .map(res => res.json());
   }
 }
