dbbe0362099c3a8e1072b4b3e877b1ef99086e71
Austin Akers <austin.akers5@gmail.com>
2019-02-05 23:29:30 -0600

    updating app

--- ng7-app/src/app/servers/servers.component.ts
+++ ng7-app/src/app/servers/servers.component.ts
@@ -6,10 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+  allowNewServer: boolean = false;
+  serverCreationStatus: string = 'No server was created';
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() { 
+    setTimeout(()=> {
+      this.allowNewServer = true
+    }, 2000);
+  }
+  ngOnInit() { 
+  }
+  
+  onCreateServer() {
+    this.serverCreationStatus = 'Server was created!';
   }
 
 }
