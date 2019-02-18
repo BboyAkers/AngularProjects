3ea6059e59815a23e6639cfec96035079bc141b2
Austin Akers <austin.akers5@gmail.com>
2019-02-18 17:45:44 -0600

    created simple server app

--- ng7-app/src/app/server/server.component.ts
+++ ng7-app/src/app/server/server.component.ts
@@ -3,6 +3,11 @@ import { Component } from '@angular/core'
 @Component({
     selector: 'app-server',
     templateUrl: './server.component.html',
+    styles: [`
+        .online {
+            color: white;
+        }
+    `]
 })
 export class ServerComponent {
     serverId: number = 10;
--- ng7-app/src/app/servers/servers.component.ts
+++ ng7-app/src/app/servers/servers.component.ts
@@ -10,6 +10,7 @@ export class ServersComponent implements OnInit {
   serverCreationStatus: string = 'No server was created';
   serverName = '';
   serverCreated = false;
+  servers = ['Testserver', 'Testserver 2']
 
   constructor() { 
     setTimeout(()=> {
@@ -21,6 +22,7 @@ export class ServersComponent implements OnInit {
   
   onCreateServer() {
     this.serverCreated = true;
+    this.servers.push(this.serverName);
     this.serverCreationStatus = `Server was created! Name is ${this.serverName}` 
   }
 
